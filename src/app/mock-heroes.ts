8745d10f7b9bd4874eeb414f295f7d420b3e2a4d
ThomasJhonson <th0m3johnson@gmail.com>
2022-11-04 07:01:00 -0700

    display a list

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,9 +1,9 @@
 import { Hero } from "./hero";
 
-// export const HEROES:Hero[]= [
-//   {id:2, name:'doctor Smith'},
-//   {id:3, name:'Mr Pak'},
-//   {id:4, name:'Dr Smile'},
-//   {id:5, name:'Bear'}
+export const HEROES:Hero[]= [
+  {id:2, name:'doctor Smith'},
+  {id:3, name:'Mr Pak'},
+  {id:4, name:'Dr Smile'},
+  {id:5, name:'Bear'}
 
-// ]
+]
