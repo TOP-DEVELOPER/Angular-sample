6cedc707db00ee5cf2c462517602e162b5ffd45a
ThomasJhonson <th0m3johnson@gmail.com>
2022-11-06 10:25:15 -0800

    complete hero-detail

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -4,6 +4,15 @@ export const HEROES:Hero[]= [
   {id:2, name:'doctor Smith'},
   {id:3, name:'Mr Pak'},
   {id:4, name:'Dr Smile'},
-  {id:5, name:'Bear'}
+  {id:5, name:'Bear'},
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
 
 ]
